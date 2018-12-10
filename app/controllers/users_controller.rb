d38430fd90d09fbf3de90c10472d88c10c00d477
yoru <bhunjio15987@gmail.com>
2018-12-10 07:20:39 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
     @users = User.all.page(params[:page])
   end
@@ -8,23 +10,37 @@ class UsersController < ApplicationController
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
     counts(@user)
   end
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
-      flash[:success] = 'ユーザ登録しました。'
+      flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
     else
-      flash[:danger] = 'ユーザの登録に失敗しました'
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
+
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
   
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
